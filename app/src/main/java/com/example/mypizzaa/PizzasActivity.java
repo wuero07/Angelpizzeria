aa2b752e5f0597f81f608a40f7ab744e824cfd8a
wuero007 <angelscp07@gmail.com>
2024-04-04 00:45:40 -0600

    quinto commit

--- app/src/main/java/com/example/mypizzaa/PizzasActivity.java
+++ app/src/main/java/com/example/mypizzaa/PizzasActivity.java
@@ -1,24 +1,126 @@
 package com.example.mypizzaa;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class PizzasActivity extends AppCompatActivity {
+
+    private ArrayList<Pizza> selectedPizzas; // Lista para almacenar las pizzas seleccionadas
+    private ArrayList<Bebida> selectedBebidas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_pizzas);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main_pizzas);
+        // Dentro de onCreate
+        Button payButton = findViewById(R.id.payButton);
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double totalPrice = calculateTotalPrice();
+                Intent intent = new Intent(PizzasActivity.this, Activity_main_pago.class);
+                intent.putStringArrayListExtra("pizzas", getPizzaNames(selectedPizzas)); // Pasar nombres de las pizzas seleccionadas
+                intent.putStringArrayListExtra("bebidas", getBebidaNames(selectedBebidas));
+                intent.putExtra("totalPrice", totalPrice);
+                startActivity(intent);
+            }
         });
+
+
+        selectedPizzas = new ArrayList<>(); // Inicializar la lista de pizzas seleccionadas
+        selectedBebidas = new ArrayList<>();
+
+
+        Button addPizza1Button = findViewById(R.id.addPizza1Button);
+        Button addPizza2Button = findViewById(R.id.addPizza2Button);
+        Button addPizza3Button = findViewById(R.id.addPizza3Button);
+
+
+        addPizza1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedPizzas.add(new Pizza("Pizza peperoni", 10.0));
+            }
+        });
+
+        addPizza2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedPizzas.add(new Pizza("Pizza Hawaiana", 12.0));
+            }
+        });
+
+        addPizza3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedPizzas.add(new Pizza("Pizza Vegetariana", 11.0));
+            }
+        });
+
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pasa a la siguiente actividad
+                Intent intent = new Intent(PizzasActivity.this, activity_main_bebidas.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button backButton = findViewById(R.id.backButton);
+        //Button payButton = findViewById(R.id.payButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double totalPrice = calculateTotalPrice();
+                Intent intent = new Intent(PizzasActivity.this, Activity_main_pago.class);
+                intent.putStringArrayListExtra("pizzas", getPizzaNames(selectedPizzas)); // Pasar nombres de las pizzas seleccionadas
+                intent.putStringArrayListExtra("bebidas", getBebidaNames(selectedBebidas));
+                intent.putExtra("totalPrice", totalPrice);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private double calculateTotalPrice() {
+        double totalPrice = 0;
+        for (Pizza pizza : selectedPizzas) {
+            totalPrice += pizza.getPrice();
+        }
+        return totalPrice;
+    }
+
+    //obtener los nombres de las pizzas seleccionadas
+    private ArrayList<String> getPizzaNames(ArrayList<Pizza> pizzas) {
+        ArrayList<String> pizzaNames = new ArrayList<>();
+        for (Pizza pizza : pizzas) {
+            pizzaNames.add(pizza.getName());
+        }
+        return pizzaNames;
+    }
+
+    private ArrayList<String> getBebidaNames(ArrayList<Bebida> bebidas) {
+        ArrayList<String> BebidaNames = new ArrayList<>();
+        for (Bebida bebida : bebidas) {
+            BebidaNames.add(bebida.getName());
+        }
+        return BebidaNames;
     }
 }
